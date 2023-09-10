45edb406b2c4be687891abc1c6cd9da45927768e
Replit user <>
2023-09-10 03:02:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -78,19 +78,7 @@ Pair *searchMap(HashMap *map, char *key) {
   long pos = hash(key, map->capacity);
   map->current = pos;
 
-  bool end = false;
-  while (!end)
-    {
-      if (map->buckets[pos] != NULL)
-      {
-        if(is_equal(map->buckets[pos]->key, key))
-          end = true;
-      }
-      else
-      {
-        pos++;
-      }
-    }
+  
 
   return map->buckets[pos];
 }
